caa323e5c30c01dcc45c06535e904c055e719b72
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-11-07 23:07:35 -0300

    código formatado

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,64 @@
 import { use, useEffect, useState } from "react";
 
-export default function App(){
+export default function App() {
     const [tarefas, novatarefa] = useState<any[]>(() => JSON.parse(localStorage.getItem("tarefas") || "[]"))
-const [input, setinput] = useState<string>("")
-useEffect(() => {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-}, [tarefas]
-)
+    const [input, setinput] = useState<string>("")
+    useEffect(() => {
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    }, [tarefas]
+    )
 
-function add(){
-    if(input.trim() === ""){
-        alert("digite uma tarefa")
-    return
+    function add() {
+        if (input.trim() === "") {
+            alert("digite uma tarefa")
+            return
+        }
+        const tasks: any = {
+            id: Date.now(),
+            text: input
+        }
+
+
+        novatarefa([...tarefas, tasks])
+        setinput("")
+        console.log(tasks)
     }
-const tasks: any = {
-    id: Date.now(),
-    text: input
-}
 
-
-novatarefa([...tarefas, tasks])
-setinput("")
-console.log(tasks)
-}
-
-function excluir(){
-   // eslint-disable-next-line no-restricted-globals
-    let sure = confirm("Tem certeza que deseja apagar tudo?")
-if(sure){
-    novatarefa([])
-}
-}
+    function excluir() {
+        // eslint-disable-next-line no-restricted-globals
+        let sure = confirm("Tem certeza que deseja apagar tudo?")
+        if (sure) {
+            novatarefa([])
+        }
+    }
 
 
 
-return(
-<section>
-    
-<h1>Lista de Tarefas</h1>
-<input value={input} onChange={(e) =>{
-    setinput(e.target.value)
-    console.log(e.target.value)
-    }}
-/>
-<button onClick={add}>Add</button>
-<ul>
-{tarefas.map((tarefa) => {
-    return <li key={tarefa.id}>
-        {tarefa.text}
-    </li>
-})
+    return (
+        <section>
 
-}
+            <h1>Lista de Tarefas</h1>
+            <input value={input} onChange={(e) => {
+                setinput(e.target.value)
+                console.log(e.target.value)
+            }}
+            />
+            <button onClick={add}>Add</button>
+            <ul>
+                {tarefas.map((tarefa) => {
+                    return <li key={tarefa.id}>
+                        {tarefa.text}
+                    </li>
+                })
 
-</ul>
-{ tarefas.length > 0 &&(
-<button onClick={excluir}>Delete All</button>
-)}
-</section>
-)
+                }
+
+            </ul>
+            {tarefas.length > 0 && (
+                <button onClick={excluir}>Delete All</button>
+            )}
+        </section>
+    )
 
 
 
